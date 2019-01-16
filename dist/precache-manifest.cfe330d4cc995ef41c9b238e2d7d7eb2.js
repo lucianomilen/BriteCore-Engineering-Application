712ff64bd39f68850315bced3353b916f08fa65b
luciano <luciano@studiomoob.com>
2019-01-15 22:02:45 -0200

    gitignore update

---
+++ dist/precache-manifest.cfe330d4cc995ef41c9b238e2d7d7eb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a18ba384c7cb70b92717",
+    "url": "/js/chunk-vendors.6a365bea.js"
+  },
+  {
+    "revision": "0b7532620aa8b5917d85",
+    "url": "/js/app.ea10d7aa.js"
+  },
+  {
+    "revision": "8a5b3335526fa9822fd69828d8ee0b1a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b7532620aa8b5917d85",
+    "url": "/css/app.19916b33.css"
+  }
+];
